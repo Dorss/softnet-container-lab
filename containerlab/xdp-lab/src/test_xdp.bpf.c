e5140b429d0651b8458003ad14316fb0d2d7d985
Andrea Mayer <andrea.mayer@uniroma2.it>
2026-05-28 20:19:54 +0200

    Add xdp-lab with generic BPF compilation toolchain
    Introduce bpf-builder container image and build-bpf.sh script for
    compiling eBPF programs using clang inside a dedicated container,
    avoiding host-side toolchain dependencies. Add xdp-lab as the first
    eBPF lab, using vmlinux.h for kernel type definitions.
    
    Signed-off-by: Andrea Mayer <andrea.mayer@uniroma2.it>

---
+++ containerlab/xdp-lab/src/test_xdp.bpf.c
@@ -0,0 +1,21 @@
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __uint(max_entries, 1);
+    __type(key, __u32);
+    __type(value, __u64);
+} packets_map SEC(".maps");
+
+SEC("xdp")
+int xdp_counter(struct xdp_md *ctx)
+{
+    __u32 key = 0;
+    __u64 *count = bpf_map_lookup_elem(&packets_map, &key);
+    if (count)
+        *count += 1;
+    return XDP_PASS;
+}
+
+char _license[] SEC("license") = "GPL";
